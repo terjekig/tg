331ada66b6a2328b7834b90bac84892391e8749b
gergely.terjeki <gergely.terjeki@aimotive.com>
2023-12-01 23:20:31 +0100

    Added input data Removed + from regex so successive numbers are separated Opening file and reading it line-by-line

--- 2023/1/1.py
+++ 2023/1/1.py
@@ -4,17 +4,14 @@ import re
 
 # --- Day 1: Trebuchet?! ---
 
-
-test_cali_text = ["1abc2",
-    "pqr3stu8vwx",
-    "a1b2c3d4e5f",
-    "treb7uchet"]
-
 res = []
 cali_sum=0
-for line in test_cali_text:
-    res = re.findall('\d+', line)
-    cali_val=int(res[0]+res[-1])
-    cali_sum+=cali_val
+
+with open('input_1','r') as f:
+    for line in f:
+        res = re.findall('\d', line)
+        cali_val=int(res[0]+res[-1])
+        cali_sum+=cali_val
+        #print(f"res: {res},  line: {line} result: {cali_val}")
 
 print(cali_sum)
