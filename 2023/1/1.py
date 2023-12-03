2cc46d7452331f68b67e9f692e0a025dde9d8261
gergely.terjeki <gergely.terjeki@aimotive.com>
2023-12-03 21:41:33 +0100

    d1p2 with test values using helper dict

--- 2023/1/1.py
+++ 2023/1/1.py
@@ -29,27 +29,31 @@ string_test=["two1nine",
 "7pqrstsixteen"]
 
 
-help_dict = {
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9',
-    'zero': '0'
+help_dict: dict[str, str] = {
+    'one':    '1',
+    'two':    '2',
+    'three':  '3',
+    'four':   '4',
+    'five':   '5',
+    'six':    '6',
+    'seven':  '7',
+    'eight':  '8',
+    'nine':   '9',
+    'zero':   '0',
+    '1' :     '1',
+    '2' :     '2',
+    '3' :     '3',
+    '4' :     '4',
+    '5' :     '5',
+    '6' :     '6',
+    '7' :     '7',
+    '8' :     '8',
+    '9' :     '9'
 }
-
 
 for line in string_test:
     res = re.findall('\d|one|two|three|four|five|six|seven|eight|nine|ten', line)
-    if res != []:
-        # TODO res[*] conversion if it is a string match
-        cali_val=int(res[0]+res[-1])
-    else:
-        cali_val=0
-    cali_sum+=cali_val
+    cali_val = int(help_dict[res[0]] + help_dict[res[-1]])
+    cali_sum += cali_val
 
 print(cali_sum)
