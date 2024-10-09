d2a0018014e4d2594d0449f9630f0187cc6fde5a
gergely.terjeki <gergely.terjeki@aimotive.com>
2024-10-09 22:41:47 +0200

    Added a template for the README.md to new.sh

--- leetcode/new.sh
+++ leetcode/new.sh
@@ -2,8 +2,35 @@
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
+README_TEMPLATE="# PROBLEM_TITLE
+
+### PROBLEM_SUMMARY
+
+problem_description
+
+
+#### Example 1:
+
+    example_1
+
+#### Example 2:
+
+    example_2
+
+#### Example 3:
+
+    example_3
+
+
+#### Constraints:
+
+    constraints
+
+"
+
+
 if [ ! -d ./$BRANCH ]; then
-    mkdir -p ./$BRANCH/{py,cpp} && echo "$BRANCH" > ./$BRANCH/README.md && echo "Folders created"
+    mkdir -p ./$BRANCH/{py,cpp} && echo "$README_TEMPLATE" > ./$BRANCH/README.md && echo "Folders created"
     for ext in py cpp; do touch ./$BRANCH/$ext/$BRANCH.$ext; done && echo "Files created"
 else
     echo "$BRANCH folder already exists"
