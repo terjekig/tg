a1ff4f1f532fd0760a133313f7ee967ef277ec54
gergely.terjeki <gergely.terjeki@aimotive.com>
2024-12-05 17:57:03 +0100

    Some progress with parsing, diff bounds

--- advent_of_code/2024/2/solution.py
+++ advent_of_code/2024/2/solution.py
@@ -11,13 +11,35 @@ def process_input(path: Path) -> list:
 
     input_list = []
 
-    with open(path, "r") as infile:
-        for line in infile:
-             for report in line.strip().split("\n"):
-                  for level in report.split():
-                    input_list.append(int(level)) # TODO lists probably won't be good enough, or use list of lists
 
-    return input_list
+    with open(path, "r") as infile:
+        n_of_safe = 0
+        increasing = False
+
+        for line in infile:
+            line_arr = []
+            safe = False
+            line_arr.append([int(x) for x in line.split()])
+
+            if line_arr[0] <= line_arr[1]: # check for equal
+                increasing = False
+            else:
+                increasing = True
+
+            prev_level = line_arr[0]
+            current_level = 0
+            for current_level in line_arr[1:]:
+                diff = current_level - prev_level
+                safe = False if ((diff < 1) or (diff > 3)) else True
+                #
+                # check direction with "increasing"
+                #
+                prev_level = current_level
+
+
+            if safe: n_of_safe += 1
+
+    return n_of_safe
 
 
 
