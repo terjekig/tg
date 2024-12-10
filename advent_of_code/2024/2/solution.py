5188f52e3b2ba539c331c673f66a9c19c615409d
gergely.terjeki <gergely.terjeki@aimotive.com>
2024-12-11 00:11:53 +0100

    Verbose solution for the test case

--- advent_of_code/2024/2/solution.py
+++ advent_of_code/2024/2/solution.py
@@ -2,42 +2,54 @@
 # Red-Nosed Reports
 
 from pathlib import Path
-
+from math import copysign
 
 def process_input(path: Path) -> list:
     '''
     I've pasted my puzzle's input to this input file
     '''
 
-    input_list = []
-
+    n_of_safe = 0
 
     with open(path, "r") as infile:
-        n_of_safe = 0
-        increasing = False
 
         for line in infile:
             line_arr = []
-            safe = False
-            line_arr.append([int(x) for x in line.split()])
+            for level in line.strip().split():
+                line_arr.append(int(level))
 
-            if line_arr[0] <= line_arr[1]: # check for equal
-                increasing = False
-            else:
-                increasing = True
+            # Check level's "direction"
+            if line_arr[0] == line_arr[1]: # skip the level, it is not safe
+                print(f"Skipping level since first 2 are equal")
+                continue
+            level_direction = copysign(1, (line_arr[1] - line_arr[0]) )
 
+            # Check conditions
             prev_level = line_arr[0]
             current_level = 0
+
+            safe = False
             for current_level in line_arr[1:]:
                 diff = current_level - prev_level
-                safe = False if ((diff < 1) or (diff > 3)) else True
-                #
-                # check direction with "increasing"
-                #
+                safe_diff = 1 <= abs(diff) <= 3
+
+                if not safe_diff:
+                    print(f"{line_arr} not safe because diff is {diff}")
+                    safe = False
+                    break
+
+                if ( level_direction != copysign(1, diff) ):
+                    print(f"{line_arr} not safe because direction is {level_direction} but current dir is {copysign(1, diff)}")
+                    safe = False
+                    break
+
+                safe = True
                 prev_level = current_level
 
 
             if safe: n_of_safe += 1
+
+
 
     return n_of_safe
 
