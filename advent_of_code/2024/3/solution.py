dd718e2cc3717bbff6894fc0d6946bf0872d01fb
gergely.terjeki <gergely.terjeki@aimotive.com>
2024-12-13 00:23:31 +0100

    Solution to day 3 part 2 in python, refactor of part 1

--- advent_of_code/2024/3/solution.py
+++ advent_of_code/2024/3/solution.py
@@ -4,57 +4,79 @@
 from pathlib import Path
 import re
 
-def process_input(path: Path) -> list:
-    '''
-    I've pasted my puzzle's input to this input file, string is fine because I'll just regex
-    '''
-
-    input_list = []
-
-    with open(path, "r") as infile:
-        for line in infile:
-             for element in line.strip().split():
-                  input_list.append(int(element))
-
-    return input_list
-
-def part_one(input):
-
+def part_one(input_file: Path, regex_patterns: dict) -> int:
+    """
+    input string has mul(digit,digit) commands, sum the prods
+    """
     # grep -oE "mul\([0-9]{1,3}\,[0-9]{1,3}\)" input | grep -oE "[0-9]{1,3}\,[0-9]{1,3}"
 
     ans = 0
-
-    with open(input, "r") as infile:
+    with open(input_file, "r") as infile:
         content = infile.read()
 
-    mul_pattern = r'mul\([0-9]{1,3},[0-9]{1,3}\)'
-    num_pattern = r'[0-9]{1,3},[0-9]{1,3}'
+    to_multiply = ' '.join(re.findall(regex_patterns["mul_pattern"], content))
+    to_multiply_strimmed = re.findall(regex_patterns["num_pattern"], to_multiply)
 
-    x = ' '.join(re.findall(mul_pattern, content))
-    x = re.findall(num_pattern, x)
-
-    for values in x:
+    for values in to_multiply_strimmed:
         A, B = re.split(",", values)
         ans += int(A)*int(B)
 
     return ans
 
 
-def part_two(input):
+def part_two(input_file: Path, regex_patterns: dict) -> int:
+    """
+    do() and don't() enable and disable every valid mul() after them
+    """
 
-    return True
+    ans = 0
+    with open(input_file, "r") as infile:
+        content = infile.read()
+
+    split_input = re.split(regex_patterns["do_dont_pattern"], content)
+
+    enabled = True
+    to_multiply_list = []
+    for match in split_input:
+
+        if match == "do()": enabled = True
+        elif match == "don't()": enabled = False
+
+        else:
+            if enabled:
+                to_multiply_list.extend(re.findall(regex_patterns["mul_pattern"], match))
+
+    to_multiply = ' '.join(to_multiply_list)
+    to_multiply_strimmed = re.findall(regex_patterns["num_pattern"], to_multiply)
+
+    for values in to_multiply_strimmed:
+        a, b = re.split(",", values)
+        ans += int(a)*int(b)
+
+    return ans
 
 
 def solution() -> None:
     """
     Solution for Day 3
     """
-    #input_data = process_input(Path(__file__).parent / "input")
-    solution_1 = part_one(Path(__file__).parent / "input")
-    #solution_2 = part_two(input_data)
+    input_path = Path(__file__).parent / "input"
+
+    do_dont_pattern = r"(do\(\)|don't\(\))" # wrapping the string in parentheses for .split returns the matched string as well
+    mul_pattern = r'mul\([0-9]{1,3},[0-9]{1,3}\)'
+    num_pattern = r'[0-9]{1,3},[0-9]{1,3}'
+
+    regex_patterns = {
+        "do_dont_pattern" : do_dont_pattern,
+        "mul_pattern"     : mul_pattern,
+        "num_pattern"     : num_pattern
+    }
+
+    solution_1 = part_one(input_path, regex_patterns)
+    solution_2 = part_two(input_path, regex_patterns)
 
     print(f"Solution 1: {solution_1}")
-    #print(f"Solution 2: {solution_2}")
+    print(f"Solution 2: {solution_2}")
 
 
 solution()
